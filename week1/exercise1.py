5f9e7b53b7c85d0cdd53e9ef1dd6b412495b5cc8
ddib-ccde <daniel.dib81@gmail.com>
2020-09-10 21:51:00 +0200

    Added exercise1.py

---
+++ week1/exercise1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Three different IP addresses from documentation prefix stored as string
+ipaddr1 = "192.0.2.1"
+ipaddr2 = "198.51.100.1"
+ipaddr3 = "203.0.113.1"
+
+# Print using f-string
+print(f"The first IP is {ipaddr1}. The second IP is {ipaddr2}. The third IP is {ipaddr3}.")
+
