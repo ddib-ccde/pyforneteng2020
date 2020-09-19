72b198d5337afcad4a40ba701fceead4a1d39bcd
ddib-ccde <daniel.dib81@gmail.com>
2020-09-19 08:09:47 +0200

    Added comments to exercise 1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -1,12 +1,26 @@
+# Open file
 f = open("show_version.txt")
+
+# Read the file into a long string
 file = f.read()
+
+# Print the contents
 print(file)
+# Print the type
 print(type(file))
+
+# Close the file
 f.close()
 
+# Open file using file handle
 with open("show_version.txt") as f:
+    # Read the lines into a list
     content = f.readlines()
 
+# Iterate over the list
 for line in content:
+    # Print each line in the list
     print(line)
+
+# Print the type, should be list
 print(type(content))
